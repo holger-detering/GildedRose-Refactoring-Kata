f6a31adedc72b653d5f1fb8d65eccc34b181b682
holger-detering <github@detering-springhoe.de>
2021-09-15 13:19:53 +0200

    add approval tests similar to cpp_texttest

--- cpp/test/cpp_catch2_approvaltest/GildedRoseCatch2ApprovalTests.cc
+++ cpp/test/cpp_catch2_approvaltest/GildedRoseCatch2ApprovalTests.cc
@@ -1,15 +1,45 @@
 #define APPROVALS_CATCH
 #include "ApprovalTests.hpp"
 
+#include <sstream>
 #include <utility>
+
 #include "GildedRose.h"
 
-std::ostream& operator<<(std::ostream& os, const Item& obj)
-{
-    return os
-        << "name: " << obj.name
-        << ", sellIn: " << obj.sellIn
-        << ", quality: " << obj.quality;
+std::ostream &operator<<(std::ostream &os, const Item &obj) {
+    return os << "name: " << obj.name << ", sellIn: " << obj.sellIn << ", quality: " << obj.quality;
+}
+
+namespace {
+std::vector<Item> Items() {
+    return std::vector<Item>{{"+5 Dexterity Vest", 10, 20},
+                             {"Aged Brie", 2, 0},
+                             {"Elixir of the Mongoose", 5, 7},
+                             {"Sulfuras, Hand of Ragnaros", 0, 80},
+                             {"Sulfuras, Hand of Ragnaros", -1, 80},
+                             {"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+                             {"Backstage passes to a TAFKAL80ETC concert", 10, 49},
+                             {"Backstage passes to a TAFKAL80ETC concert", 5, 49},
+                             {"Conjured Mana Cake", 3, 6}};
+}
+} // namespace
+
+TEST_CASE("ApprovingText") {
+    auto items = Items();
+    GildedRose app(items);
+
+    std::stringstream out_stream;
+    for (int day = 0; day <= 30; ++day) {
+        out_stream << "----- day " << day << " -----\n";
+        out_stream << "name, sellIn, quality\n";
+        for (const auto &item : items) {
+            out_stream << item << "\n";
+        }
+        out_stream << '\n';
+        app.updateQuality();
+    }
+
+    ApprovalTests::Approvals::verify(out_stream.str());
 }
 
 TEST_CASE("Verify combinations with Foo")
@@ -25,7 +55,5 @@ TEST_CASE("Verify combinations with Foo")
         return items[0];
     };
 
-    ApprovalTests::CombinationApprovals::verifyAllCombinations(
-            f,
-            names, sellIns, qualities);
+    ApprovalTests::CombinationApprovals::verifyAllCombinations(f, names, sellIns, qualities);
 }
--- cpp/test/cpp_googletest_approvaltest/GildedRoseGoogletestApprovalTests.cc
+++ cpp/test/cpp_googletest_approvaltest/GildedRoseGoogletestApprovalTests.cc
@@ -1,24 +1,53 @@
-// Include header files for test frameworks 
-#include <gtest/gtest.h>
+// Include header files for test frameworks
 #include <ApprovalTests.hpp>
+#include <gtest/gtest.h>
+
+#include <sstream>
 #include <utility>
 
-// Include code to be tested
+// Include code under test
 #include "GildedRose.h"
 
-std::ostream& operator<<(std::ostream& os, const Item& obj)
-{
-    return os
-        << "name: " << obj.name
-        << ", sellIn: " << obj.sellIn
-        << ", quality: " << obj.quality;
+std::ostream &operator<<(std::ostream &os, const Item &obj) {
+    return os << "name: " << obj.name << ", sellIn: " << obj.sellIn << ", quality: " << obj.quality;
 }
 
-TEST(GildedRoseApprovalTests, VerifyCombinations) {
+namespace {
+std::vector<Item> Items() {
+    return std::vector<Item>{{"+5 Dexterity Vest", 10, 20},
+                             {"Aged Brie", 2, 0},
+                             {"Elixir of the Mongoose", 5, 7},
+                             {"Sulfuras, Hand of Ragnaros", 0, 80},
+                             {"Sulfuras, Hand of Ragnaros", -1, 80},
+                             {"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+                             {"Backstage passes to a TAFKAL80ETC concert", 10, 49},
+                             {"Backstage passes to a TAFKAL80ETC concert", 5, 49},
+                             {"Conjured Mana Cake", 3, 6}};
+}
+} // namespace
 
-    std::vector<string> names { "Foo" };
-    std::vector<int> sellIns { 1 };
-    std::vector<int> qualities { 1 };
+TEST(GildedRoseApprovalTests, Approvingtext) { // NOLINT(cert-err58-cpp)
+    auto items = Items();
+    GildedRose app(items);
+
+    std::stringstream out_stream;
+    for (int day = 0; day <= 30; ++day) {
+        out_stream << "----- day " << day << " -----\n";
+        out_stream << "name, sellIn, quality\n";
+        for (const auto &item : items) {
+            out_stream << item << "\n";
+        }
+        out_stream << '\n';
+        app.updateQuality();
+    }
+
+    ApprovalTests::Approvals::verify(out_stream.str());
+}
+
+TEST(GildedRoseApprovalTests, VerifyCombinations) { // NOLINT(cert-err58-cpp)
+    std::vector<string> names{"Foo"};
+    std::vector<int> sellIns{1};
+    std::vector<int> qualities{1};
 
     auto f = [](string name, int sellIn, int quality) {
         vector<Item> items = {Item(std::move(name), sellIn, quality)};
@@ -27,8 +56,5 @@ TEST(GildedRoseApprovalTests, VerifyCombinations) {
         return items[0];
     };
 
-    ApprovalTests::CombinationApprovals::verifyAllCombinations(
-            f,
-            names, sellIns, qualities);
-
+    ApprovalTests::CombinationApprovals::verifyAllCombinations(f, names, sellIns, qualities);
 }
